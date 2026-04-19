f78d84c37637233267181e9d75c6f22b66705c10
madhurichinthala17 <madhurichinthala2000@gmail.com>
2026-04-18 20:16:44 -0500

    chunking with context

--- FinanChatBot/utils/chunking.py
+++ FinanChatBot/utils/chunking.py
@@ -1,21 +1,22 @@
 import utils.documentloader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+from Helpers.splitter import split_by_sections
 filtered_docs = utils.documentloader.filtered_docs
 
 text_splitter = RecursiveCharacterTextSplitter(
     # Set a really small chunk size, just to show.
     chunk_size=1000,
-    chunk_overlap=100
+    chunk_overlap=400
     #s_separator_regex=False, This always defaults sto False
 )
+#This one works but it is not efficient as the context might not be stored effectively
+# texts = text_splitter.split_documents(filtered_docs)
 
-texts = text_splitter.split_documents(filtered_docs)
-print(texts[0].page_content)
+final_content = "\n".join(doc.page_content for doc in filtered_docs)
 
-print("-----------")
+sections = split_by_sections(final_content)
 
-print(texts[1].page_content)
-
-print("-----------")
-
-print(texts[2].page_content)
+final_chunks = []
+for section in sections:
+    chunks = text_splitter.split_text(section)
+    final_chunks.extend(chunks)
